e5c5b3a669985104be719b78a7774ca5cd8bdddf
tuxfoo <tuxfoo@librem.one>
2021-03-30 18:41:23 +1300

    Using async to resolve issues on windows

--- odysee-tip-listener.py
+++ odysee-tip-listener.py
@@ -2,8 +2,13 @@ import requests
 import json
 import re
 import time
+import aiohttp
+import asyncio
 
 # Basic script to listen for tips with a valid signing channel on stream and activate a stream alert using simple-alerts.
+# If you followed the video tutorial, this script has had some improvements
+# so make sure you also install the following 2 modules
+# python -m pip install aiohttp asyncio
 # Change this to your stream claim_id for each stream
 claim_id = "e78621e7ea5ddb580147f8be5cd8abf8fee35d2d"
 
@@ -13,7 +18,7 @@ tips = []
 # Make sure to change these to your alert names that you have set up in simple-alerts
 # First do is equals too (special numbers for unique alerts such as 420)
 # Then go by decending amount
-def alert(name, amount):
+async def alert(name, amount):
     url = 'http://localhost:9090/simple-alerts/alert'
     amount = round(float(amount), 2)
     #Default Alert name, change this to your alert name if you are using simple-alerts
@@ -31,35 +36,46 @@ def alert(name, amount):
         alertName = "tada30"
     elif amount >= 15:
         alertName = "tada15"
+
     alert = {"name":alertName, "message":"(" + name + ") tipped (" + str(amount) + ") LBC"}
-    req = requests.post(url, data=alert)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=alert) as response:
+            response = await response.text()
+    #req = requests.post(url, data=alert)
     return
 
 # Function to get username/channel name of tipper
-def get_channel(id):
+async def get_channel(id):
     url = "http://localhost:5279/"
     payload = '{"method": "claim_search", "params": {"claim_id": "' + id + '"}}'
-    response = requests.post(url, data=payload)
-    result = json.loads(response.text)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=payload) as response:
+            response = await response.text()
+    #response = requests.post(url, data=payload)
+    result = json.loads(response)
     return result["result"]["items"][0]["name"]
 
-while True:
+async def main():
     url = "http://localhost:5279/"
     payload = '{"method": "support_list", "params": {"claim_id": "' + claim_id + '", "received": ' + received + ', "page_size":' + '10' + '}}'
     header = "{'content-type': 'applicati1on/json', 'Accept-Charset': 'UTF-8'}"
-    response = requests.post(url, data=payload)
-    result = json.loads(response.text)
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=payload) as response:
+            response = await response.text()
+    result = json.loads(response)
     #check if tip is new
     for tip in result["result"]["items"]:
         if tip["txid"] not in tips:
             tips.append(tip["txid"])
-            timestamp = int(tip["timestamp"])
-            # Ignore if tip is older than 2 hours
-            if timestamp < int(time.time()) - 7200:
-                try:
-                    channel = get_channel(tip["signing_channel"]["channel_id"])
+            try:
+                timestamp = int(tip["timestamp"])
+                # Ignore if tip is older than 2 hours
+                if timestamp < int(time.time()) - 7200:
+                    channel = await get_channel(tip["signing_channel"]["channel_id"])
                     print(channel + ": " + tip["amount"])
-                    alert(channel, tip["amount"])
-                except:
-                    print("No signing channel")
-    time.sleep(15)
+                    await alert(channel, tip["amount"])
+            except:
+                print("No signing channel")
+
+while True:
+    asyncio.run(main())
